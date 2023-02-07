2fbc81877ac8f4aea78c6755d4d28578d192b548
bjelicamarko <marko.bjelica19@gmail.com>
2023-02-07 17:16:53 +0100

    Classification and regression models added.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
@@ -7,10 +6,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.svm import SVC
 from sklearn import metrics
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LinearRegression, Ridge
 
 from util import *
+
+import warnings
+warnings.filterwarnings('once')
 
 def train_test_validation_with_scaling(features, target, scaler):
     xtrain, xtest, ytrain, ytest = train_test_split(features, target, test_size=0.2, random_state=40)
@@ -23,27 +26,38 @@ def train_test_validation_with_scaling(features, target, scaler):
 def cross_validation_with_scaling():
     pass
 
+def classification(xtrain, xtest, ytrain, ytest, classification_models):
+    for i in range(len(classification_models)):
+        classification_models[i].fit(xtrain, ytrain)
+        print(f'{classification_models[i]} : ')
+        print("F Measure: " + 
+            str(metrics.f1_score(ytest, classification_models[i].predict(xtest),  average='weighted')))
+        print()
+
+def regression(xtrain, xtest, ytrain, ytest, regression_models):
+    for i in range(len(regression_models)):
+        regression_models[i].fit(xtrain, ytrain)
+        print(f'{regression_models[i]} : ')
+        print("RMSE: " + 
+            str(metrics.mean_squared_error(ytest, regression_models[i].predict(xtest),  squared=True)))
+        print()
+
 def main():
     df = pd.read_csv('prepared_dataset.csv')
     features = df.drop(['quality', 'best quality'], axis=1)
-    target = df['best quality']
-  
+    target = df['quality']
+    
     # standardization
     xtrain, xtest, ytrain, ytest = train_test_validation_with_scaling(features, target, StandardScaler())
 
     # normalization
     #xtrain, xtest, ytrain, ytest = train_test_validation_with_scaling(features, target, MinMaxScaler())
 
-    models = [SVC(kernel='rbf'), GradientBoostingRegressor(), LinearRegression()]
-    for i in range(len(models)):
-        models[i].fit(xtrain, ytrain)
-        print(f'{models[i]} : ')
-        print('Training Accuracy : ', metrics.roc_auc_score(ytrain, models[i].predict(xtrain)))
-        print('Validation Accuracy : ', metrics.roc_auc_score(
-            ytest, models[i].predict(xtest)))
-        print()
+    regression_models = [GradientBoostingRegressor(), LinearRegression(), Ridge()]
+    regression(xtrain, xtest, ytrain, ytest, regression_models)
   
-    # print(metrics.classification_report(ytest, svm.predict(xtest)))
+    classification_models = [SVC(kernel='rbf'), RandomForestClassifier(), GaussianNB()]
+    classification(xtrain, xtest, ytrain, ytest, classification_models)
 
 if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def dataset_info(df):
     df.info()
     print(df.describe().T)
@@ -17,3 +19,41 @@ def preparing_dataset(df):
     df['best quality'] = [1 if x > 5 else 0 for x in df.quality]
     df.replace({'white': 1, 'red': 0}, inplace=True)
     return df
+
+def five_fold_split(date_frame, seed=None):
+    np.random.seed(seed)
+    perm = np.random.permutation(date_frame.index)
+    m = len(date_frame.index)
+
+    block1_end = int(0.2 * m)
+    block2_end = int(0.2 * m + block1_end)
+    block3_end = int(0.2 * m + block2_end)
+    block4_end = int(0.2 * m + block3_end)
+    block5_end = int(0.2 * m + block4_end)
+
+    block1 = date_frame.iloc[perm[:block1_end]]
+    block2 = date_frame.iloc[perm[block1_end: block2_end]]
+    block3 = date_frame.iloc[perm[block2_end: block3_end]]
+    block4 = date_frame.iloc[perm[block3_end: block4_end]]
+    block5 = date_frame.iloc[perm[block4_end:]]
+
+    blocks = []
+
+    blocks.append(block1)
+    blocks.append(block2)
+    blocks.append(block3)
+    blocks.append(block4)
+    blocks.append(block5)
+
+    return blocks
+
+def train_validate_cross(blocks, k):
+    train = pd.DataFrame()
+    validate = pd.DataFrame()
+
+    for i, block in enumerate(blocks):
+        if k != i:
+            train = train.append(block)
+        else:
+            validate = validate.append(block)   
+    return train, validate
